80efe60b1ca38ea1f75567a3f70dd52384b3b38f
ElielLopez <elielsw1@gmail.com>
2020-11-29 14:15:43 +0200

    working but not all the times.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-
-#include <iostream>
 #include <vector>
 #include <stdlib.h>
 #include <time.h>
@@ -28,74 +26,73 @@ Point** generate(Point center,int R, size_t size){
 int main(){
 
     //----------- tests ---------------
-    Point a(6,4);
-    Point b(3,5);
-    calcDistance(a,b);
-
-    Point c(-1,-1);
-    Point d(-10,-3);
-    calcMiddle(c,d);
+//    Point a(3,5);
+//    Point b(5,7);
+//    calcDistance(a,b); //2.82
+//    createTwoPointCircle(a, b);
+//
+//    Point cPoint(4, 3);
+//    Point d(-10,-3);
+//    //calcDistance(c, a);
+//    calcDistance(cPoint, b);
+//    createTwoPointCircle(cPoint, b);
+//    isBigger(createTwoPointCircle(cPoint, b).center, d, createTwoPointCircle(cPoint, b).radius);
 
 
     // -------------------------------
-//    srand (time(NULL));
-//    const size_t N=250;
-//    float R=10+rand()%1000;
-//    float cx=-500+rand()%1001;
-//    float cy=-500+rand()%1001;
-//    Point** ps=generate(Point(cx,cy),R,N);
-//
-//    // your working copy
-//    Point** ps_copy=new Point*[N];
-//    for(size_t i=0;i<N;i++)
-//        ps_copy[i]=new Point(ps[i]->x,ps[i]->y);
-//
-//    auto start = high_resolution_clock::now();
-//    Circle c=findMinCircle(ps_copy,N);
-//    auto stop = high_resolution_clock::now();
-//
-//    if((int)c.radius>(int)R)
-//        cout<<"you need to find a minimal radius (-40)"<<endl;
-//
-//    bool covered=true;
-//    for(size_t i=0;i<N && covered;i++){
-//        float x2=(c.center.x-ps[i]->x)*(c.center.x-ps[i]->x);
-//        float y2=(c.center.y-ps[i]->y)*(c.center.y-ps[i]->y);
-//        float d=sqrt(x2+y2);
-//        if(d>c.radius+1)
-//            covered=false;
-//    }
-//    if(!covered)
-//        cout<<"all points should be covered (-45)"<<endl;
-//
-//    auto duration = duration_cast<microseconds>(stop - start);
-//    int stime=duration.count();
-//    cout<<"your time: "<<stime<<" microseconds"<<endl;
-//    if(stime>3000){
-//        cout<<"over time limit ";
-//        if(stime<=3500)
-//            cout<<"(-5)"<<endl;
-//        else if(stime<=4000)
-//            cout<<"(-8)"<<endl;
-//        else if(stime<=6000)
-//            cout<<"(-10)"<<endl;
-//        else cout<<"(-15)"<<endl;
-//    }
-//
-//    for(size_t i=0;i<N;i++){
-//        delete ps[i];
-//        delete ps_copy[i];
-//    }
-//    delete[] ps;
-//    delete[] ps_copy;
+    srand (time(NULL));
+    const size_t N=250;
+    float R=10+rand()%1000;
+    float cx=-500+rand()%1001;
+    float cy=-500+rand()%1001;
+    Point** ps=generate(Point(cx,cy),R,N);
+
+    // your working copy
+    Point** ps_copy=new Point*[N];
+    for(size_t i=0;i<N;i++) {
+        ps_copy[i] = new Point(ps[i]->x, ps[i]->y);
+        //cout<<ps_copy[i]->x<<"  "<<ps_copy[i]->y<<endl;
+    }
+
+    auto start = high_resolution_clock::now();
+    Circle c=findMinCircle(ps_copy,N);
+    auto stop = high_resolution_clock::now();
+
+    if((int)c.radius>(int)R)
+        cout<<"you need to find a minimal radius (-40)"<<endl;
+
+    bool covered=true;
+    for(size_t i=0;i<N && covered;i++){
+        float x2=(c.center.x-ps[i]->x)*(c.center.x-ps[i]->x);
+        float y2=(c.center.y-ps[i]->y)*(c.center.y-ps[i]->y);
+        float d=sqrt(x2+y2);
+        if(d>c.radius+1)
+            covered=false;
+    }
+    if(!covered)
+        cout<<"all points should be covered (-45)"<<endl;
+
+    auto duration = duration_cast<microseconds>(stop - start);
+    int stime=duration.count();
+    cout<<"your time: "<<stime<<" microseconds"<<endl;
+    if(stime>3000){
+        cout<<"over time limit ";
+        if(stime<=3500)
+            cout<<"(-5)"<<endl;
+        else if(stime<=4000)
+            cout<<"(-8)"<<endl;
+        else if(stime<=6000)
+            cout<<"(-10)"<<endl;
+        else cout<<"(-15)"<<endl;
+    }
+
+    for(size_t i=0;i<N;i++){
+        delete ps[i];
+        delete ps_copy[i];
+    }
+    delete[] ps;
+    delete[] ps_copy;
 
     cout<<"done"<<endl;
     return 0;
 }
-
-
-
-//int main() {
-//    std::cout << "Hello smallest circle in the world" << std::endl;
-//    return 0;
-//}
--- minCircle.h
+++ minCircle.h
@@ -7,6 +7,10 @@
 #define SMALLESTCIRCLEPROBLEM_MINCIRCLE_H
 
 #include <iostream>
+#include <algorithm>
+#include <utility>
+#include <math.h>
+#include <vector>
 
 using namespace std;
 
@@ -25,10 +29,8 @@ public:
 };
 // --------------------------------------
 
-// implement
-Circle findMinCircle(Point** points,size_t size){
-    return Circle(Point(0,0),0);
-}
+
+const float INF = 1e18;
 
 // return the middle point by equation: x = (x1-x2) / 2, y = (y1-y2) / 2.
 Point calcMiddle(Point a, Point b) {
@@ -66,6 +68,8 @@ float calcDistance(Point a, Point b) {
 
     x = ax - bx;
     y = ay - by;
+//    x = fabs(ax - bx);
+//    y = fabs(ay - by);
 
     x = pow(x, 2);
     y = pow(y, 2);
@@ -76,19 +80,6 @@ float calcDistance(Point a, Point b) {
     return distance;
 }
 
-// checks if a given point is outside the current circle.
-bool isBigger(Point center, Point a, float radius) {
-
-    float dist;
-    bool isBiggerThenR = false;
-
-    dist = calcDistance(center, a);
-    if(radius < dist) isBiggerThenR = true;
-
-    return isBiggerThenR;
-
-}
-
 // given two points, calculate the center point and the radius and creates a small circle
 // that the two points are on the circle
 Circle createTwoPointCircle(Point a, Point b) {
@@ -96,15 +87,122 @@ Circle createTwoPointCircle(Point a, Point b) {
     float radius;
 
     auto center = calcMiddle(a, b);
-    radius = calcDistance(a, b);
-    radius /= 2;
+    radius = calcDistance(a, b); // diameter
+    radius /= 2; // radius
 
-    //Point center(x, y);
     Circle c(center, radius);
+
     return c;
 }
 
+// returns the center point of a circle, given 2 points.
+Point getCenterOfCircle(Point a, Point b) {
 
+    float c, d, e;
 
+    c = pow(a.x, 2) + pow(a.y, 2);
+    d = pow(b.x, 2) + pow(b.y, 2);
+    e = a.x * b.y - a.y * b.x;
+
+    return {(b.y * c - a.y * d) / (2 * e), (a.x * d - b.x * c) / (2 *e)};
+}
+
+// returns the center point of a circle, given 4 floats
+Point getCenterOfCircle(float bx, float by, float cx, float cy) {
+
+    float b, c, d;
+
+    b = pow(bx, 2) + pow(by, 2);
+    c = pow(cx, 2) + pow(cy, 2);
+    d = (bx * cy) - (by * cx);
+
+    return {(cy * b - by * c) / (2 * d), (bx * c - cx * b) / (2 * d)};
+}
+
+Circle threePointsCircle(Point a, Point b, Point c) {
+    Point center = getCenterOfCircle(b.x - a.x, b.y - a.y, c.x - a.x, c.y - a.y);
+    center.x += a.x;
+    center.y += a.y;
+    return {center, calcDistance(center, a)};
+}
+
+// checks if a given point is inside a certain circle.
+bool isInside(Circle c, Point a) {
+    return calcDistance(c.center, a) <= c.radius;
+}
+
+// functions to check if all points are inside the circle.
+bool isValid(Circle c, Point** points) {
+    for(int i = 0; i < 250; i++) {
+        if(!isInside(c, Point(points[i]->x, points[i]->y))) return false;
+    }
+    return true;
+}
+
+// return a minimum circle that enclose all points in 2D plane.
+Circle findMinCircle(Point** points,size_t size) {
+
+    // trivial mec- minimum enclosing circle.
+    if(size == 0) return {{0, 0}, 0};
+    if(size == 1) return {Point(points[0]->x, points[0]->y), 0};
+    if(size == 2) return createTwoPointCircle(Point(points[0]->x, points[0]->y),
+                                              Point(points[1]->x, points[1]->y));
+
+    // if the number of the points are 4 or more, set a circle from the first 3 points.
+    // every boundary point is on the circle and not inside.
+    Point boundary1(points[0]->x, points[0]->y);
+    Point boundary2(points[1]->x, points[1]->y);
+    Point boundary3(points[2]->x, points[2]->y);
+
+    Circle mecOfThreePoints = threePointsCircle(boundary1, boundary2, boundary3);
+
+    for(int i = 4; i < size; i++) {
+
+        Point p(points[i]->x, points[i]->y);
+
+        // check if point is inside circle, if not, enter.
+        if(!isInside(mecOfThreePoints, p)) {
+
+            // check distances between the current point to the boundaries points.
+            float distPTob1 = calcDistance(p, boundary1);
+            float distPTob2 = calcDistance(p, boundary2);
+            float distPTob3 = calcDistance(p, boundary3);
+
+            // b1 is the furthest.
+            if(distPTob2 < distPTob1 && distPTob3 < distPTob1) {
+                if(distPTob2 < distPTob3) {
+                    boundary3 = p; // b1, b2, p
+                    //boundary3 = {points[i]->x, points[i]->y}; // b1, b2, p
+                }
+                if(distPTob3 < distPTob2) {
+                    boundary2 = p; // b1, p, b3
+                }
+            }
+
+            // b2 is the furthest.
+            if(distPTob1 < distPTob2 && distPTob3 < distPTob2) {
+                if(distPTob3 < distPTob1) {
+                    boundary1 = p; // b1, b2, p
+                }
+                if(distPTob1 < distPTob3) {
+                    boundary3 = p; // p, b2, b3
+                }
+            }
+
+            // b3 is the furthest.
+            if(distPTob1 < distPTob3 && distPTob2 < distPTob3) {
+                if(distPTob1 < distPTob2) {
+                    boundary2 = p; // p, b2, b3
+                }
+                if(distPTob2 < distPTob1) {
+                    boundary1 = p; // b1, p, b3
+                }
+            }
+        }
+        mecOfThreePoints = threePointsCircle(boundary1, boundary2, boundary3);
+    }
+
+    return mecOfThreePoints;
+}
 
 #endif //SMALLESTCIRCLEPROBLEM_MINCIRCLE_H
