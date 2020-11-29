818a558355fc2bbe4318e32dd8ac4628038f9f08
ElielLopez <elielsw1@gmail.com>
2020-11-29 17:18:34 +0200

    improving running time to 30ms+- after inserting another condition in the first loop. grade- 100.

--- minCircle.h
+++ minCircle.h
@@ -1,7 +1,3 @@
-// 309288777
-//
-// Created by eliel on 26/11/2020.
-//
 
 #ifndef SMALLESTCIRCLEPROBLEM_MINCIRCLE_H
 #define SMALLESTCIRCLEPROBLEM_MINCIRCLE_H
@@ -82,7 +78,6 @@ float calcDistance(Point a, Point b) {
     return distance;
 }
 
-
 // given two points, calculate the center point and the radius and creates a small circle
 // that the two points are on the circle
 Circle createTwoPointCircle(Point a, Point b) {
@@ -96,38 +91,6 @@ Circle createTwoPointCircle(Point a, Point b) {
     Circle c(center, radius);
 
     return c;
-}
-
-// return center point given 2 points.
-Point getCenterOfCircle(Point a, Point b) {
-
-    float c, d, e;
-
-    c = pow(a.x, 2) + pow(a.y, 2);
-    d = pow(b.x, 2) + pow(b.y, 2);
-    e = a.x * b.y - a.y * b.x;
-
-    return {(b.y * c - a.y * d) / (2 * e), (a.x * d - b.x * c) / (2 *e)};
-}
-
-// return center point given 4 float numbers.
-Point getCenterOfCircle(float ax, float ay, float bx, float by) {
-
-    float b, c, d;
-
-    b = pow(ax, 2) + pow(ay, 2);
-    c = pow(bx, 2) + pow(by, 2);
-    d = ax * by - ay * bx;
-
-    return {(by * b - ay * c) / (2 * d), (ax * c - bx * b) / (2 * d)};
-}
-
-// return a circle from 3 Points according to the equation.
-Circle createThreePointsCircle(Point a, Point b, Point c) {
-    Point center = getCenterOfCircle(b.x - a.x, b.y - a.y, c.x - a.x, c.y - a.y);
-    center.x += a.x;
-    center.y += a.y;
-    return {center, calcDistance(center, a)};
 }
 
 // checks if a given Point is outside the boundaries of the circle.
@@ -151,9 +114,6 @@ Circle findMinCircle(Point** points,size_t size) {
     if(size == 1) return {Point(points[0]->x, points[0]->y), 0};
     if(size == 2) return createTwoPointCircle(Point(points[0]->x, points[0]->y),
                                               Point(points[1]->x, points[1]->y));
-    if(size == 3) return createThreePointsCircle(Point(points[0]->x, points[0]->y),
-                                                 Point(points[1]->x, points[1]->y),
-                                                 Point(points[2]->x, points[2]->y));
 
     // default circle.
     Circle mec = {{0, 0}, INF};
@@ -161,15 +121,18 @@ Circle findMinCircle(Point** points,size_t size) {
     // for every 2 points, create a circle and then check its validity and size.
     // if smaller then the current circle- update it.
     for(int i = 0; i < size; i++) {
-        for(int j = i + 1; j < size; j++) {
 
-            // this condition helps reduce the time significantly
-            // because its ignoring points that are inside the circle.
-            if(!isInsideCircle(mec, Point(points[j]->x, points[j]->y))) {
-                Circle tmp = createTwoPointCircle(Point(points[i]->x, points[i]->y),
-                                                  Point(points[j]->x, points[j]->y));
-                if(tmp.radius < mec.radius && isValidMEC(tmp, points, size)) {
-                    mec = tmp;
+        // this condition helps reduce the time significantly
+        // because its ignoring points that are inside the circle.
+        if(!isInsideCircle(mec, Point(points[i]->x, points[i]->y))) {
+            for(int j = i + 1; j < size; j++) {
+
+                if(!isInsideCircle(mec, Point(points[j]->x, points[j]->y))) {
+                    Circle tmp = createTwoPointCircle(Point(points[i]->x, points[i]->y),
+                                                      Point(points[j]->x, points[j]->y));
+                    if(tmp.radius < mec.radius && isValidMEC(tmp, points, size)) {
+                        mec = tmp;
+                    }
                 }
             }
         }
